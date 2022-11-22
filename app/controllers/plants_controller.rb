d7f01cacf571ab2b4c6efc2c8a3f320e1c8def92
Tracie35 <kariukitracie@gmail.com>
2022-11-22 14:36:24 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,27 +1,29 @@
 
 class PlantsController < ApplicationController
     
-     # GET '/plants'
-    def index
-        plants = Plant.all
-        render json: plants,  only: [:name, :image, :price] status: :ok
+    def index 
+        plants = Plant.all 
+        render json: plants, except: [:created_at, :updated_at]
     end
 
-     # GET 'plants/:id'
-    def show
-        plant = Plant.find_by(id: params[:id])
-         if plant
-            render json: plant, status: 200
+    def show 
+        plant = Plant.find_by_id(params[:id])
+        if plant 
+            render json: plant, except: [:created_at, :updated_at]
         else
-            render json: {"error": "Plant not found"}, status: 404
+            render json: {error: 'Plant not found'}, status: :not_found
         end
     end
-     
-    
-    # POST '/plants'
+
     def create
-        plant = Plant.create(plant_params)
+        plant = Plant.create(plants_params)
         render json: plant, status: :created
+    end
+
+    private
+
+    def plants_params 
+        params.permit(:name, :image, :price)
     end
 end
 
